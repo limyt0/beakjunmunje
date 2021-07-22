015118dce22436d3d9e01061abf86eb9a3f6f596
임영택 <dam45@naver.com>
2021-07-22 17:36:00 +0900

    level1문제 풀었음

---
+++ src/level1/AmulB.java
@@ -0,0 +1,15 @@
+package level1;
+
+import java.util.Scanner;
+
+public class AmulB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a*b);
+	}
+
+}
