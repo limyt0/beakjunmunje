51266c085f7e13173629f396475b77ead87baf4c
임영택 <dam45@naver.com>
2021-07-28 23:05:16 +0900

    간단한 문제

---
+++ src/level3/AplusB3.java
@@ -0,0 +1,19 @@
+package level3;
+
+import java.util.Scanner;
+
+public class AplusB3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i =0; i<T; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			int num = A +B;
+			System.out.println(num);
+		}
+
+	}
+
+}
