dc0b0ff7f63d943fe6ca15169524f7f06f83935a
임영택 <dam45@naver.com>
2021-08-10 22:30:56 +0900

    수정필요. 예시로 주어진 테케만 통과함.

--- src/study/a0805To0812/BJ11725_트리의부모찾기.java
+++ src/study/a0805To0812/BJ11725_트리의부모찾기.java
@@ -1,29 +1,40 @@
 package study.a0805To0812;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Node{
-	private Node parents;//부모 노드.
-	private int value;
-	public Node(Node parents, int value) {
-		//super();
-		this.parents = parents;
-		this.value = value;
-	}
-	
-}
-
+//수정 필요
 public class BJ11725_트리의부모찾기 {
-
+	
 	public static void main(String[] args) throws IOException {
-		ArrayList<Node> node = new ArrayList<>(); 
-		Node root = new Node(null,1);//루트 노드
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//StringTokenizer st= new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
 		
+		int [] node = new int[N+1];//index에 해당하는 부모를 담을 배열.
+		//node[1] = -1;//트리 노드 기본값 삽입. 이거 안주면 값 꼬임
+		for(int i =0; i<N-1; i++) { //N개중 1은 제외
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int parent = Integer.parseInt(st.nextToken());//부모값.
+			int idx = Integer.parseInt(st.nextToken());//인덱스
+			
+			if(idx == 1) {//1이 무조건 부모.
+				node[parent] = idx; 
+			}else if(parent == 1) {
+				node[idx] = parent; //1이 무조건 부모.2
+			}else if(node[idx] ==0) { //현재 값이 없으니 넣는 경우
+				node[idx] = parent;//인덱스가 0부터 시작하므로. N을 초과하지 않으려면 -1해야됨
+			}else {//0이 아닐 경우. 값이 이미 있으므로 노드가 뒤집힌 경우
+				node[parent] = idx;
+			}
+		}
+		for(int i = 2; i<=N; i++) {//2번째(=idx-1)이므로 idx=1부터 시작.
+			sb.append(node[i]).append("\n");
+		}
+		
+		System.out.println(sb);
+		br.close();
 	}
 
 }
