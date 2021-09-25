04c3dab3962aa1e7951ae0319d9d39e0e779fe3d
임영택 <dam45@naver.com>
2021-09-25 21:29:06 +0900

    .

--- src/study/a0923To/BJ_15684_사다리조작.java
+++ src/study/a0923To/BJ_15684_사다리조작.java
@@ -15,17 +15,17 @@ public class BJ_15684_사다리조작 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		H = Integer.parseInt(st.nextToken());
-		arr= new int[H+1][N+1]; //?
+		arr= new int[H+1][N+1]; //가로선 위치갯수, 세로선 수.
 		
 		int row, col;
 		for(int i = 0; i<M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
 			row = Integer.parseInt(st.nextToken());
 			col = Integer.parseInt(st.nextToken());
-			arr[row][col] = 1;
-			arr[row][col+1] = 2;//??
+			arr[row][col] = 1; //가로선 시작점
+			arr[row][col+1] = 2;//가로선 도착점
 		}
-		for(int i = 0; i<= 3; i++) {
+		for(int i = 0; i<= 3; i++) { //
 			result = i;
 			dfs(1, 0);
 			if(finish) break;
@@ -42,22 +42,22 @@ public class BJ_15684_사다리조작 {
 		}
 		for(int i = start; i<H+1;i++) {
 			for(int j = 0; j<N; j++) {
-				if(arr[i][j] == 0 && arr[i][j+1] == 0) {
-					arr[i][j] = 1;
-					arr[i][j+1] = 2;
-					dfs(i, cnt + 1);
-					arr[i][j] = arr[i][j+1] = 0;
+				if(arr[i][j] == 0 && arr[i][j+1] == 0) { //
+					arr[i][j] = 1; //가로선 시작
+					arr[i][j+1] = 2;//가로선 끝
+					dfs(i, cnt + 1);//
+					arr[i][j] = arr[i][j+1] = 0; //다시 0으로 만듦??
  				}
 			}
 		}
 	}
-	private static boolean check() {
+	private static boolean check() { //사다리 이동 체크?
 		for(int i = 1; i<=N; i++) {
 			int row =1, col = i;
 			for(int j = 0; j<H; j++) {
-				if(arr[row][col] == 1) col++;
-				else if(arr[row][col] == 2) col--;
-				row++;
+				if(arr[row][col] == 1) col++; //왼쪽->오른쪽으로 이동
+				else if(arr[row][col] == 2) col--; //오른쪽->왼쪽으로 이동
+				row++; //아래로 이동
 			}
 			if(col != i) return false;
 		}
