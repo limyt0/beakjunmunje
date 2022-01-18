a2e159b1b0ee03537b3df19e1ecc4c73b08de525
limyt0 <80087763+limyt0@users.noreply.github.com>
2022-01-19 00:01:43 +0900

    팩

---
+++ src/level10/BJ10872팩토리얼.java
@@ -0,0 +1,24 @@
+package level10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ10872팩토리얼 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int Nfac = fact(N);
+		System.out.println(Nfac);
+		br.close();
+
+	}
+
+	private static int fact(int n) {
+		if(n <= 1) return 1;
+		return n * fact(n-1);
+	}
+
+}
