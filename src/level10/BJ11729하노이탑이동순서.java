d4a05e2d6bbd22670357ab98e32d27f039e2c039
limyt0 <80087763+limyt0@users.noreply.github.com>
2022-01-24 19:43:33 +0900

    commit

---
+++ src/level10/BJ11729하노이탑이동순서.java
@@ -0,0 +1,13 @@
+package level10;
+
+import java.io.*;
+
+
+public class BJ11729하노이탑이동순서 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		System.out.println();
+		br.close();
+	}
+}
