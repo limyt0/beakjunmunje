788ff678f0fcb62efd75686a1d167f1f1edfafdf
limyt0 <80087763+limyt0@users.noreply.github.com>
2022-01-29 09:54:42 +0900

    BJ 문제 풀기

---
+++ src/level08_math1/BJ1193분수찾기.java
@@ -0,0 +1,16 @@
+package level08_math1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ1193분수찾기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		System.out.println();
+		br.close();
+	}
+}
